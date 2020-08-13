4245618d009cc5c4ea1283ccff4ee0c21e7920ef
Tomas Poveda <tpovedatd@gmail.com>
2020-08-14 00:59:44 +0200

    Added basic Hello World Maya plugin Python implementation

---
+++ python/helloworld.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+"""
+Python Hello World Command plugin for Maya
+"""
+
+
+from __future__  import print_function, division, absolute_import, unicode_literals
+
+
+__author__ = "Tomas Poveda"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpoveda@cgart3d.com"
+
+# region Imports
+import maya.OpenMayaMPx as OpenMayaMPx
+# endregion
+
+class HelloWorldPyCmd(OpenMayaMPx.MPxCommand, object):
+
+    command = "HelloWorldPy"
+
+    def __init__(self):
+        super(HelloWorldPyCmd, self).__init__()
+
+    def doIt(self, args):
+        print('Hello World')
+
+def creator():
+    return OpenMayaMPx.asMPxPtr(HelloWorldPyCmd())
+
+def initializePlugin(mobj):
+    mplugin = OpenMayaMPx.MFnPlugin(mobj)
+    try:
+        mplugin.registerCommand(HelloWorldPyCmd.command, creator)
+    except:
+        raise Exception('Failed to register command: {}'.format(HelloWorldPyCmd.command))
+
+def uninitializePlugin(mobj):
+    mplugin = OpenMayaMPx.MFnPlugin(mobj)
+    try:
+        mplugin.deregisterCommand(HelloWorldPyCmd.command)
+    except:
+        raise Exception('Failed to unregister command: {}'.format(HelloWorldPyCmd.command))
+
+HelloWorldPyCmd().doIt("")
